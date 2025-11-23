bea1f1d9557ab863c834fbdb94f36dca24ae8f42
Yopi <myopisugara23@gmail.com>
2025-11-23 15:38:07 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from 'path';
 import { defineConfig } from "vite";
 
-const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
+const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname;
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Nexxport/",  // <= WAJIB ADA!
   plugins: [react(), tailwindcss()],
   build: {
     sourcemap: true,
